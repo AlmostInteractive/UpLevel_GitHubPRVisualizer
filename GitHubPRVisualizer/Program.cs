67936f304861e70e88bf831964308d4e7c3728e5
Steven J <3893631+AlmostInteractive@users.noreply.github.com>
2024-08-31 10:51:22 -0600

    Add a command line validator to screen for invalid --stuck-pr values

--- GitHubPRVisualizer/Program.cs
+++ GitHubPRVisualizer/Program.cs
@@ -5,15 +5,30 @@ namespace GitHubPRVisualizer;
 
 internal static class Program {
     private static async Task<int> Main(string[] args) {
-        ;
+        // we'll use this one in the validator below
+        Option stuckPrsOption = new Option<int>("--stuck-prs",
+            description: "Show PRs stuck in review for more than a specified number of days)");
+        
         var rootCommand = new RootCommand {
             new Argument<string>("repository", "GitHub repository in the format 'owner/repo'"),
-            new Argument<string>("token", "GitHub Personal Access Token (PAT)"),
+            new Argument<string>("token", "GitHub Personal Access Token"),
             new Option<bool>("--week-stats", "Show the number of PRs opened/closed in the last week"),
-            new Option<int>("--stuck-prs", "Show PRs stuck in review for more than a specified number of days)")
+            stuckPrsOption
         };
 
         rootCommand.Description = "GitHub Pull Request Visualizer";
+
+        rootCommand.AddValidator(result => {
+            try {
+                var optionResult = result.FindResultFor(stuckPrsOption);
+                if (optionResult != null && optionResult.GetValueOrDefault<int>() < 0) {
+                    result.ErrorMessage = $"The value for stuck-prs cannot be negative, received {optionResult.GetValueOrDefault<int>()}";
+                }
+            }
+            catch (Exception _) {
+                // prevents a crash but it's unnecessary to handle the exception, it's already caught and handled nicely 
+            }
+        });
 
         rootCommand.Handler = CommandHandler.Create<string, string, bool, int>(async (repository, token, weekStats, stuckPrs) => {
             try {
